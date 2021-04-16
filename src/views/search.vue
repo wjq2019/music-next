2b7f0895aa73884265605867b741deca0f4a78ad
wjq2019 <892462352@qq.com>
2021-04-16 15:57:59 +0800

    tab

---
+++ src/views/search.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>搜索页面</div>
+</template>
+<script>
+
+export default ({
+    data () {
+        return {
+            name: 'top'
+        }
+    }
+})
+</script>
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/singer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>歌手页面</div>
+</template>
+<script>
+
+export default ({
+    data () {
+        return {
+            name: 'top'
+        }
+    }
+})
+</script>
+<style lang="sass" scoped>
+
+</style>
